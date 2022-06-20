bfdb4632167b32cd1f46a3b0f803da5940682850
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-06-20 18:48:05 +0200

    fix gh-pages

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
+  basePath: "/mc-website",
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/mc-website",
+  //       basePath: false,
+  //       permanent: false,
+  //     },
+  //     // {
+  //     //   source: "/mc-website",
+  //     //   destination: "/mc-website",
+  //     //   basePath: false,
+  //     //   permanent: true,
+  //     // },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
